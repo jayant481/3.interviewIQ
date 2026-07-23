1291ec5eef1e0963fb23e3be58426bffb20428d5
jayant481 <jayantmahajan985@gmail.com>
2026-07-24 00:06:33 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://three-interviewiq-37h3.onrender.com"
+export const ServerUrl  = "https://three-interviewiq-1-8q12.onrender.com"
 
 function App() {
 
